323c985a44fc2e1490b5e52799026104b29aac0a
belendominguezda <mdominguezdealzaga@udesa.edu.ar>
2023-04-03 11:52:04 -0300

    cambie algunos hipervinculos

--- proyectoIntegradorPrograII/controllers/loginController.js
+++ proyectoIntegradorPrograII/controllers/loginController.js
@@ -2,9 +2,6 @@ let loginController = {
     index: function (req,res){
         return res.render ('login')
     },
-    logeado : function (req,res){
-        return res.render ('headerLogueado')
-    },
 };
 
 module.exports = loginController
--- proyectoIntegradorPrograII/routes/login.js
+++ proyectoIntegradorPrograII/routes/login.js
@@ -5,6 +5,5 @@ let loginController = require ('../controllers/loginController')
 //Rutas
 
 router.get ('/', loginController.index)
-router.get ('/logeado', loginController.logeado)
 
 module.exports = router;
