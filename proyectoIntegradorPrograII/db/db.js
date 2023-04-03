5dc23c89c72159a3f63fb9163c23d60c800c9722
serena-russo <srusso@udesa.edu.ar>
2023-04-03 11:23:45 -0300

    base de datos

---
+++ proyectoIntegradorPrograII/db/db.js
@@ -0,0 +1,75 @@
+let db = {
+    registracion: [
+        {
+            email: "juanperez@gmail.com",
+            usuario: "juanperez1",
+            contrasena: "JPerez2023",
+            fechaDeNacimiento: "03/03/2003",
+            nroDoc: "45730730",
+            fotoPerfil: "../publicimages/users/default-image.jpg"
+        }],    
+    productos: [
+        {//1
+        imagen: "../publicimages/users/default-image.jpg",
+        nombreDelProducto: "GREATEST HITS (Octubre 1981) - Queen - Libro con vinilo",
+        descripcion: "Greatest Hits es el primer álbum compilatorio de la banda de rock Queen, aparecido en 1981. En él se reúnen los éxitos de la banda entre 1974 y 1980. Esta lista corresponde a la edición en el Reino Unido. En otros países tuvo diferentes canciones.",
+        fechaDeCarga: "11/10/2022"
+    },{//2
+        imagen: "../publicimages/users/default-image.jpg",
+        nombreDelProducto: "Ed sheeran - x lpx2 - libro con vinilo",
+        descripcion: "Ed Sheeran - X LPX2 - Libro con vinilo",
+        fechaDeCarga: "	1/1/2012"
+    },{//3
+        imagen: "../publicimages/users/default-image.jpg",
+        nombreDelProducto: "Led Zeppelin Physical graffiti - libro con vinilo",
+        descripcion: "Physical Graffiti es el nombre del sexto álbum de estudio de la banda británica Led Zeppelin. Fue publicado el 24 de febrero de 1975, siendo la primera publicación del grupo con su propio sello, denominado Swan Song Records.",
+        fechaDeCarga: "	1/1/2012"
+    },{//4
+        imagen: "../publicimages/users/default-image.jpg",
+        nombreDelProducto: "Dua lipa - Dua lipa - Libro con vinilo",
+        descripcion: "Dua Lipa es una cantante, compositora, modelo y actriz británica de origen albanokosovar. Después de trabajar como modelo, firmó con Warner Bros. Records en 2014 y lanzó su álbum debut homónimo en 2017.",
+        fechaDeCarga: "	1/1/2022"
+    },{//5
+        imagen: "../publicimages/users/default-image.jpg",
+        nombreDelProducto: "Led Zeppelin: houses of the holy- Libro con vinilo",
+        descripcion: "Houses of the Holy es el quinto álbum de la banda británica Led Zeppelin. Rompe con la tradición de los discos anteriores al tener un título no homónimo.",
+        fechaDeCarga: "1/9/2022"
+    },{//6
+        imagen: "../publicimages/users/default-image.jpg",
+        nombreDelProducto: "Madonna true blue - libro con vinilo",
+        descripcion: "True Blue es el tercer álbum de estudio de la cantante estadounidense Madonna, publicado el 30 de junio de 1986 por la compañía discográfica Sire Records.",
+        fechaDeCarga: "1/1/2020"
+    },{//7
+        imagen: "../publicimages/users/default-image.jpg",
+        nombreDelProducto: "Bruno mars unorthodox jukebox - libro con vinilo",
+        descripcion: "Unorthodox Jukebox —en español: Rocola no convencional— es el segundo álbum de estudio del cantante estadounidense Bruno Mars, lanzado el 6 de diciembre de 2012 por Atlantic Records. El 4 de diciembre de 2012, el álbum estaba disponible para escucharse completo una semana antes de su lanzamiento.",
+        fechaDeCarga: "1/1/2022"
+    },{//8
+        imagen: "../publicimages/users/default-image.jpg",
+        nombreDelProducto: "La música de sandro. cómo se hicieron sus canciones",
+        descripcion: "Con su inconfundible voz, su carisma en el escenario y en la pantalla grande, pero sobre todo con sus canciones y sus discos, Sandro se convirtió en uno de los grandes ídolos de la música en el mundo hispanohablante. Desde sus comienzos como rocanrolero a principios de los sesenta hasta su posterior triunfo como baladista romántico, su vasta producción atravesó los más diversos ritmos y vertientes de la canción popular del siglo XX",
+        fechaDeCarga: "1/1/2016"
+    },{//9
+        imagen: "../publicimages/users/default-image.jpg",
+        nombreDelProducto: "Tom Petty and the heartbreakers - you are gonna get it - libro con vinilo",
+        descripcion: "Tom Perty and the Heartbreakers - You Are Gonna Get It - Libro con Vinilo",
+        fechaDeCarga: "1/1/2012"
+    },{//10
+        imagen: "../publicimages/users/default-image.jpg",
+        nombreDelProducto: "Red hot chili peppers - blood sugar sex magik x2 - libro con vinilo",
+        descripcion: "Red Hot Chili Peppers - Blood Sugar Sex Magik X2 - Libro con vinilo",
+        fechaDeCarga: "1/1/2012"
+    }
+    ],
+
+    comentarios: [
+        {
+            nombreUsuario: "",
+            textoComentario: "",
+            imagenPerfil: "no es necesario que coloques una imagen de perfil. Podés dejar la que está ahora en la maqueta. Punto 3 - Personalización del sitio"
+        }
+    ]
+
+}
+
+module.exports = db;
