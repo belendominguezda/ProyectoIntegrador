168f9e509eb9ae708752344172779fe96cbde110
belendominguezda <mdominguezdealzaga@udesa.edu.ar>
2023-04-08 00:53:30 -0300

    intente completar logeado / no logeado. hice formularios

--- proyectoIntegradorPrograII/controllers/loginController.js
+++ proyectoIntegradorPrograII/controllers/loginController.js
@@ -1,7 +1,23 @@
 let loginController = {
-    index: function (req,res){
-        return res.render ('login')
-    },
-};
+    index: function obtenerDatos (req,res){
+        //Capturo el valor de 'usuario' y 'contraseña'
+
+        //NO SE SI ESTO VA
+        //usuario = document.getElementById("usuario").value;
+        //contrasena = document.getElementById("contrasena").value;
+
+        let user = req.query.usuario;
+        let contr = req.query.contraseña;
+
+        if (user == null && contr == null){
+            logeado = false
+        } else{
+            logeado = true
+        }
+        return res.render ('login',{
+            logeado : logeado,
+        })
+    }    
+    }
 
 module.exports = loginController
