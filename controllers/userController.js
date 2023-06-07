b5e5043b5e885b9c521d99c0bb518329ef1c5f3f
belendominguezda <mdominguezdealzaga@udesa.edu.ar>
2023-06-07 20:26:59 -0300

    avance con register

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,10 @@
-let db = require('../db/db')
-//let op = db.Sequelize.Op;
-//let bcriptjs = require('bcryptjs');
+let db = require('../database/models');
+let op = db.Sequelize.Op;
+//let bcrypt = require('bcryptjs');
 
 let userController = {
     register: function (req,res){
-        let user = req.query.usuario;
+       /*  let user = req.query.usuario;
         let contr = req.query.contrasena;
         console.log(user)
 
@@ -12,10 +12,10 @@ let userController = {
             logeado = false
         } else{
             logeado = true
-        }
+        } */
         return res.render ('register',{
-            logeado : logeado,
-            informacion : db.lista
+            //logeado : logeado,
+            //informacion : db.lista
         })
     },
     profile: function (req,res){
@@ -45,15 +45,40 @@ let userController = {
         })
     },store: function(req,res){
         let form = req.body
-        return res.send(req.body)
+
+        let user = {
+            email : form.email,
+            usuario : form.usuario,
+          //contrasena : bcriptjs.hashSync(form.contrasena, 10),
+            fechaNacimiento : form.fechaNacimiento,
+            documento : form.documento,
+            fotoPerfil : 'default-image.png'}
+        
+        //Mandar mensaje de los errores
+        let buscaEmail = {
+            where: [{email: user.email}]
+        }
+        db.Usuario.findOne(buscaEmail)
+            .then (function(resultado){
+                let errors = {};
+            if (user.email == ""){
+                errors.message = "El email está vacío";
+                res.locals.errors = errors;
+                return res.render ('register');
+            } /* else if (user.contrasena.length < 3){
+                errors.message = "La clave debe tener como mínimo 3 caracteres"
+                res.locals.errors = errors;
+                return res.render ('register');
+            } */ else if (user.usuario == ""){
+                errors.message = "El campo del usuario es obligatorio"
+                res.locals.errors = errors;
+                return res.render ('register');}
+            })
+        
          //Encriptar la contraseña antes de guardar en la base de datos.
-        //  let user = {
-        //     email: form.email,
-        //     usuario: form.usuario,
-        //     //contrasena: bcriptjs.hashSync(form.contrasena, 10),
-        //     fechaNacimiento: form.fechaNacimiento,
-        //     documento: form.documento,
-        //     fotoPerfil: 'default-image.png'}
+        
+
+        //Buscar el usuario en la db    
 
          //Usar un método de Sequelize para guardar datos.
         // db.User.create(user) //Pasar un objeto literal con los datos a guardar.
